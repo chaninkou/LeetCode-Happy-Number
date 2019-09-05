53b30d97a6a4726da64ef374502a46b9f61f62bd
chaninkou <michaelkoulove@yahoo.com>
2019-09-05 01:24:48 -0700

    Ok problem, updated comment on how it add the square numbers

--- src/happy/CheckHappyNumberFunction.java
+++ src/happy/CheckHappyNumberFunction.java
@@ -14,15 +14,20 @@ public class CheckHappyNumberFunction {
         while(inLoop.add(n)){
             squareSum = 0;
             while(n > 0){
+                // Getting single digit
                 remain = n % 10;
                 
+                // Adding the square numbers
                 squareSum = squareSum + remain * remain;
                 
+                // Decrease the number of digit by 1 place
                 n = n / 10;
             }
+            // If its 1, then it is a happy number
             if(squareSum == 1){
                 return true;
             } else {
+                //  Checking next sum now
                 n = squareSum;
             }
         
